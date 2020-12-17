12f9f5a0e378b4648e5ceae0b52b026e127af431
JosephEvans92 <dillon.evans90@yahoo.com>
2020-12-16 22:16:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-graphic-design-qw3hk6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-graphic-design-qw3hk6)
---
+++ src/app/products.ts
@@ -0,0 +1,151 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/* Main page (Flushing, NY) data */
+export const r1 = {
+
+    /* Hero, NYC photo */
+    "image": {
+    "city": "https://i.pinimg.com/originals/90/7b/18/907b18cffbe0409cc15b577c87ab886e.jpg",
+
+    "train":"https://i1.wp.com/ladobny.com/wp-content/uploads/2018/02/7-train.jpg?fit=750%2C555&ssl=1",
+
+    "food":"https://adventure.com/wp-content/uploads/2017/06/Various-Cantonese-dim-sum-in-Flushing-photo-credit-Erik-Trinidad.jpg",
+
+    "library":"https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/queens/flushing/flushing-library/qnsmade-onthegrid-flushing-flushing-library-0002_299006722e285f47655d17d1c9136337.jpg",
+
+    "history": "https://i.pinimg.com/originals/80/ae/d3/80aed3b97c03a7cf834f03a01b51d46c.jpg",
+
+    "mall": "https://ds4.cityrealty.com/img/3944853d88603206e1abfcd917ecfa44189a28f0+w+h+0+60/sky-view-parc-tower-1-40-26-college-point-boulevard-01.jpg",
+
+    "globe": "https://media.timeout.com/images/100432609/630/472/image.jpg",
+
+    "park":"https://upload.wikimedia.org/wikipedia/commons/a/ad/VIEW_OF_UNISPHERE_FROM_SKYLINE_TOWER%2CFLUSHING%2CQUEENS%2CNY_-_panoramio.jpg",
+
+    /* Main Street photo */
+    "main": "https://queenspost.com/wp-content/uploads/2020/09/DOT.jpg",
+
+    "garden": "https://queensbotanical.org/wp-content/uploads/2018/02/Tuliptime3.png",
+
+    "map": "https://comicvine1.cbsistatic.com/uploads/scale_medium/0/4378/5794709-flushing-ny-map.jpg",
+
+    "dinner": "https://cdn.vox-cdn.com/thumbor/EkEiwTf1IA8q7q1Y_ZviZ9NBDYI=/0x0:2048x1360/2220x1665/filters:focal(861x517:1187x843):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/63100521/FlushingMapLittleLamb.0.jpg",
+
+    "hotel":"https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2014/09/21/1547/NYCZF-P002-Exterior-Street.jpg/NYCZF-P002-Exterior-Street.16x9.jpg",
+
+    
+    /* citifield */
+    "citifield": "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/citi17951.jpg",
+
+    /* About - Main page data */
+    "about": "Multicultural Flushing is home to the city’s largest Chinatown, with bustling sidewalks, a tumult of shops, and eateries serving soup dumplings to Taiwanese pancakes. Korean and Indian cultures are well represented too. Built in 1862, Flushing Town Hall now hosts jazz concerts, and several historic homes date to the 17th and 18th centuries. The Queens Botanical Garden is a 39-acre oasis with a focus on native plants.",
+
+    /* Botanical Garden paragraph main page */
+    "gardenDetails": "Queens Botanical Garden is a botanical garden located at 43-50 Main Street in Flushing, Queens, New York City. The 39-acre site features rose, bee, herb, wedding, and perennial gardens; an arboretum; an art gallery; and a LEED-certified Visitor & Administration Building.",
+
+
+    "mapDetails": "Flushing is located in the heart of Queens. It accessible from the LIE, Grand Central pkwy, Van Wyck expwy and Corss Island Parkway.",
+
+    "dinnerDetails": "Little Sheep Mongolian Hot Pot is a restaurant with a purpose. That purpose is to introduce all the of the great culinary traditions and flavors of Mongolia to the people of North America. Little Sheep creates an authentic experience for diners.",
+
+    "hotelDetails": "Enjoy a variety of features during your stay at Hyatt Place Flushing / LaGuardia Airport, including free shuttle service to and from the LaGuardia airport.",
+
+  },
+
+  /* Botanical Garden and restaurants data */
+
+  "garden": {
+
+    "g1": "https://i.pinimg.com/originals/24/32/0f/24320fa3871fdac06c148aee9cfa4cde.jpg",
+
+    "g2":"https://www.urbanadvantagenyc.org/wp-content/uploads/2017/11/QBG-Pic6.png",
+
+    "g3":"https://images.glaciermedia.ca/polopoly_fs/1.1017800.1398988993!/fileImage/httpImage/image.jpg_gen/derivatives/landscape_804/web-0503-pathway-jpg.jpg",
+
+    "g4":"https://ih1.redbubble.net/image.554206901.9742/flat,1000x1000,075,f.u7.jpg",
+
+    "restaurant1":"https://images.nycgo.com/image/fetch/q_70,w_900/https://www.nycgo.com/images/uploads/articles/Flushing_food_guide/Queens-Night-Market-flushong-queens-nyc_MG_4488.jpg",
+
+    "restaurant2":"https://images.nycgo.com/image/fetch/q_70,w_900/https://www.nycgo.com/images/uploads/articles/Flushing_food_guide/asian-jewels-seafood-restaurant-flushong-queens-nyc-julienneschaer_004.jpg",
+
+    "restaurant3":"http://www.queensbuzz.com/files/image/article/thumb_415.jpg",
+
+    "restaurant4":"http://www.queensbuzz.com/files/image/article/thumb_894.jpg",
+
+
+
+    "paragraph1": "A string of Korean restaurants line Flushing’s Northern Boulevard and its surrounding blocks. Slightly off the main drag is Mapo, a tidy corner spot set up for group dining. For tableside grilling, it’s hard to do better; the kalbi and pork belly get highest marks.",
+
+    "paragraph2":"Find a lively dim sum banquet hall with all the traditional trays flying around, and feast on har gow (shrimp dumplings), steamed pork buns, rice noodle rolls and custard tarts.",
+
+    "paragraph3":"You might want to take this review with you next time you head into Flushing, because everyone of these dishes was a hit.",
+
+    "paragraph4":"A sort of modern eclectic restaurant that borrows the best from the panoply of cuisines around the world.",
+
+    /* Did you know? info  */
+    "paragraph5":"In 1664, the English took control of New Amsterdam, ending Dutch control of the New Netherland colony, and renamed it the Province of New York. When Queens County was established in 1683, the Town of Flushing was one of the original five towns which comprised the county."
+
+
+ },
+
+}
+
+
+/* Hotels and map data on last page of pagination */
+export const r2 = {
+  "hotel": {
+
+  "hotel1":"https://media-cdn.tripadvisor.com/media/photo-s/1b/36/8e/02/exterior.jpg",
+
+  "hotel2":"https://media-cdn.tripadvisor.com/media/photo-s/13/6d/75/f2/ramada-flushing-queens.jpg",
+
+  "hotel3":"https://media-cdn.tripadvisor.com/media/photo-s/1b/65/e4/60/exterior.jpg",
+  
+  "hotel4":"https://media.timeout.com/images/103749568/630/472/image.jpg",
+
+
+  "hyatt":"Our brand new, ultramodern 168 room Hyatt Place Flushing/LaGuardia Airport hotel is built atop three-glass levels of world class retail shopping, a variety of ethnic and American restaurants, a separate office condominium tower and 43 luxury furnished residential condominiums. Exclusively accessible only to hotel guests, the hotel also features an expansive roof-top outdoor garden and restaurant & lounge with spectacular panoramic views of the Manhattan skyline.",
+
+  "ramada":"Ramada by Wyndham Flushing Queens is an excellent choice for travelers visiting Flushing, offering a family-friendly environment alongside many helpful amenities designed to enhance your stay.",
+
+  "sheraton":"Enjoy a warm welcome at beautiful Sheraton LaGuardia East Hotel. Located in Flushing, New York, our Chinatown Queens hotel is just 3 miles away from the LaGuardia airport, accessible by our complimentary shuttle.",
+
+  "parc":"Experience culture, style, and sophistication at our hotel in Flushing, NY. Thoughtfully situated near Citi Field, USTA, and downtown Flushing. Modern comfort and affordability await with stunning NYC skyline views, complimentary buffet breakfast, free wifi, and two onsite restaurants. Ascend to our rooftop bar for sweeping views of LaGuardia Airport, Citi Field, and the Manhattan skyline. Walking distance to both the Long Island Railroad and famed number 7 train.",
+
+
+  "map":"https://static01.nyt.com/packages/images/newsgraphics/2011/0123-nyc-ethnic-neighborhoods-map/nyt-2010-nyc-mosaic-map.png",
+
+
+  "mapDescription":"Flushing contains numerous residential subsections, and its diversity is reflected by the numerous ethnic groups that reside there. Flushing is served by several stations on the Long Island Rail Road's Port Washington Branch, as well as the New York City Subway's IRT Flushing Line 7 and 7 trains, which has its terminus at Main Street.Flushing is located in Queens Community District 7, and its ZIP Codes are 11354, 11355, and 11358. It is patrolled by the New York City Police Department's 109th Precinct.",
+
+
+  "details":"During the 19th century, as New York City continued to grow in population and economic vitality, so did Flushing. Its proximity to Manhattan was critical in its transformation into a fashionable residential area. In 1837, the Village of Flushing was incorporated within the Town of Flushing. The Village of Flushing, surrounded by nondescript flowers. No other emblem or flag is known to have been used."
+
+
+
+
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
